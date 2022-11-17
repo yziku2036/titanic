118eca2ca240d1b2d444e098c6740f8e579d6624
yziku <yziku2036@gmail.com>
2022-11-17 14:36:54 +0900

    pclass_sex comp

--- relation_pclass_sex.py
+++ relation_pclass_sex.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing  import LabelEncoder
@@ -16,20 +15,24 @@ ax1 = fig.add_subplot(131)
 ax2 = fig.add_subplot(132)
 ax3 = fig.add_subplot(133)
 
-prepared_set=train_set.fill_null(train_set["Age"].mean())
-print(type(train_set['Age']))
-PClassPlot=prepared_set.select(pl.col(["Survived","Pclass","Age"])).groupby("Pclass")
+
+labelencoder=LabelEncoder()
 
 
-print(PClassPlot.mean())
+sex_converted_df=train_set.select(pl.when(pl.col("Sex")=="male").then(1).otherwise(0).alias("Sex"))
+prepared_set=sex_converted_df.with_column(train_set["Pclass"])
+
+PClassPlot=prepared_set.select(pl.col(["Pclass","Sex"])).groupby("Pclass")
+
 PClassPlot=PClassPlot.mean()
 
+print(PClassPlot)
 
-ax1.bar(x=PClassPlot["Pclass"], height=PClassPlot["Survived"])
-ax1.set_ylabel('Survival Rate')
+
+ax1.bar(x=PClassPlot["Pclass"], height=PClassPlot["Sex"])
+ax1.set_ylabel('Proportion of Male')
 ax1.set_xlabel('PClass')
-#ax1.set_xticks(PClassPlot.index)
 ax1.set_yticks(np.arange(0, 1.1,.1))
-ax1.set_title("Class and Survival Rate")
+ax1.set_title("Class and Gender Ratio")
 
 plt.show() 
