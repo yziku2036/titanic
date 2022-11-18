c8ca428c655c9a37b2a73dd5dc341fa8bd36e378
yziku <yziku2036@gmail.com>
2022-11-18 13:54:22 +0900

    all_survived comp

--- relation_all_survived.py
+++ relation_all_survived.py
@@ -9,7 +9,7 @@ def main():
 # データを読み込む
     train_set = pl.read_csv('data/train.csv')
     test_set = pl.read_csv('data/test.csv')
-    #prepared_set=train_set.fill_null(train_set["Age"].median())
+    
     prepared_set=preproc(train_set)
     #fig0=plt.figure(figsize=(8,4))
     fig1 = plt.figure(figsize=(12, 12),tight_layout=True)
@@ -17,49 +17,56 @@ def main():
     ax1 = fig1.add_subplot(331)
     ax2 = fig1.add_subplot(332)
     ax3 = fig1.add_subplot(333)
-    ax4=    fig1.add_subplot(334)
-    ax5=fig1.add_subplot(335)
-    ax6=fig1.add_subplot(336)
-    ax7=fig1.add_subplot(337)
-    ax8=fig1.add_subplot(338)
-    ax9=fig1.add_subplot(339)
-
-    #survived_age(train_set,ax0)
-   # pclass_age(train_set,ax1)
-    sex_survived(prepared_set,ax1)
-    all_survived(prepared_set,ax2,"Age")
-    all_survived(prepared_set,ax3,"Pclass")
-    #all_survived(prepared_set,ax4,"Fare")
-    #all_survived(prepared_set,ax5,"Fare")
-    #all_survived(prepared_set,ax6,"Fare")
-    #all_survived(prepared_set,ax7,"Fare")
-    #all_survived(prepared_set,ax8,"Fare")
-    #all_survived(prepared_set,ax9,"Fare")
+    ax4 = fig1.add_subplot(334)
+    ax5 = fig1.add_subplot(335)
+    ax6 = fig1.add_subplot(336)
+    ax7 = fig1.add_subplot(337)
+    #all_survived(prepared_set,ax1,"PassengerId")
+    all_survived(prepared_set,ax1,"Pclass")
+    sex_survived(prepared_set,ax2)
+    all_survived(prepared_set,ax3,"Age")
+    all_survived(prepared_set,ax4,"SibSp")
+    all_survived(prepared_set,ax5,"Parch")
+    all_survived(prepared_set,ax6,"Fare")
+    embarked_survived(prepared_set,ax7)
 
     plt.show() 
 
 def preproc(data):
     age_fixed=data.fill_null(data['Age'].median())
-    prepared_set=age_fixed
+    prepared_set=age_fixed.with_column(age_fixed["Embarked"].fill_null("S"))
+    
     return prepared_set
 
 
-def sex_survived(data,ax):
-# Sex （性別）の値を処理(female=0/male=1に変換)
+def embarked_survived(data,ax):
+    #性別がmale/femaleの文字列なため処理が面倒 male=1 female=0に置き換える
     
+    emb_converted_df=data.with_column(pl.col("Embarked").str.replace("S","0").str.replace("Q","1").str.replace("C","2"))
+    prepared_set=emb_converted_df.with_column(pl.col("Embarked").cast(pl.Int64))
+
+    plot=prepared_set.select(pl.col(["Embarked","Survived"])).groupby("Embarked").mean().sort("Embarked")
+    print(plot)
+    ax.bar(x=plot["Embarked"],height=plot["Survived"])
+    ax.set_ylabel('Survived')
+    ax.set_xlabel('Embarked')
+    ax.set_xticks([0,1,2],['Southampton','Queenstown','Cherbourg'])
+    ax.set_yticks(np.arange(0, 1.1,.1))
+    ax.set_title('Embarked Place and Survival Rate')
+
+def sex_survived(data,ax):
+
     #性別がmale/femaleの文字列なため処理が面倒 male=1 female=0に置き換える
     sex_converted_df=data.select(pl.when(pl.col("Sex")=="male").then(1).otherwise(0).alias("Sex"))
     #性別を変換したものと客室の等級の情報を合体する
     prepared_set=sex_converted_df.with_column(data["Survived"])
-    print(prepared_set)
-    plot=prepared_set.select(pl.col(["Sex","Survived"])).groupby("Sex").mean()
-    print(plot)
-    ax.bar(x=plot["Sex"], height=plot["Survived"])
+    plot=prepared_set.select(pl.col(["Sex","Survived"])).groupby("Sex").mean().sort("Sex")
+    ax.bar(x=[0,1],height=plot["Survived"])
     ax.set_ylabel('Survived')
     ax.set_xlabel('Gender')
-#    ax.set_xticks(plot["Sex"])
-    #ax.set_yticks(np.arange(0, 1.1,.1))
-    ax.set_title('Sex and Survival Rate')
+    ax.set_xticks([0,1],['female','male'])
+    ax.set_yticks(np.arange(0, 1.1,.1))
+    ax.set_title('Gender and Survival Rate')
 
 
 def all_survived(data,ax,tag):
